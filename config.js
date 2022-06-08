e6d878d375516682fd26e482b7a9ab1bc2020ce2
kangdeploy <106664499+kangdeploy@users.noreply.github.com>
2022-06-08 22:38:49 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ let fs = require('fs')
 let chalk = require('chalk')
 
 global.owner = [
-  ['6283811034750'],
+  ['62 83116164360', 'Owner', true]
   ['18312576749', 'Owner', true]
   // [number, dia creator/owner?, dia developer?]
 ] // Put your number here
