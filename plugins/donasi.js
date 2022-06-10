48920c67e0aff4497dfe2099c469bca888e31d29
Fardevv2 <107226281+Fardevv2@users.noreply.github.com>
2022-06-11 06:40:30 +0700

    Add files via upload

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,4 +1,6 @@
 let handler = async m => m.reply(`
+let img = 'https://i.ibb.co/305yt26/bf84f20635dedd5dde31e7e5b6983ae9.jpg'
+
 SCRIPT NYA : https://youtu.be/RMixpZRoDOk
 
 Donasi Pulsa 
@@ -6,7 +8,7 @@ Donasi Pulsa
  •  [0896-5436-0447]
 
 `.trim()) // Tambah sendiri kalo mau
-handler.help = ['donasi']
+handler.help = ['sc']
 handler.tags = ['info']
 handler.command = /^dona(te|si)$/i
 
