8851b5ae661c157f0d636d8cd68d23fe97ce61b2
Fardevv2 <107226281+Fardevv2@users.noreply.github.com>
2022-06-11 10:19:09 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,10 +1,11 @@
 let handler = async m => m.reply(`
 
 SCRIPT NYA : https://youtu.be/RMixpZRoDOk
-
-Donasi Pulsa 
- •  [083811034750]
- •  [0896-5436-0447]
+Jangan lupa like nya
+Donasi
+ • 083168004413 dana/gopay/Ovi
+ • 083811034750 Pulsa [Axis]
+ • 0896-5436-0447 [pulsa]
 
 `.trim()) // Tambah sendiri kalo mau
 handler.help = ['sc']
