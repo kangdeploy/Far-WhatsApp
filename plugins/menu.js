b6ee8edb315b3f813f50ef93be2e3f97ec6648d1
Fardevv2 <107226281+Fardevv2@users.noreply.github.com>
2022-06-11 17:39:07 +0700

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -159,7 +159,7 @@ let handler = async (m, { conn, usedPrefix: _p }) => {
     text = text.replace(new RegExp(`%(${Object.keys(replace).sort((a, b) => b.length - a.length).join`|`})`, 'g'), (_, name) => '' + replace[name])
     conn.sendHydrated(m.chat, text.trim(), 'Spam bot = baned!!!', null, 'https://youtu.be/RMixpZRoDOk', 'Script bot', '', '', [
       ['Pemilik bot', '/owner'],
-      ['Script bot', '/donasi'],
+      ['Sewa bot', '/sewa'],
     ], m)
     /*let url = `https://telegra.ph/file/00492e657e2d1eeb8b569.jpg`.trim()
     let res = await fetch(url)
